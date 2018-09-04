755c0bdcb1c6d4cb691a3a59ba0cccbbcdaa75bf
王世隆 <547463736@qq.com>
2018-09-04 21:33:18 +0800

    初始化

---
+++ packages/moduleA/index.js
@@ -0,0 +1,3 @@
+const lodash = require('lodash')
+
+exports.getRandom = (min, max) => lodash.random(min, max)
---
+++ packages/moduleB/index.js
@@ -0,0 +1,3 @@
+const { getRandom } = require('moduleA')
+
+console.log(getRandom(1, 10))
